3681233bd441db9118e39554f82a8e13d22e510d
Lee Jae Hyun <myleje@gmail.com>
2021-09-07 10:34:33 +0900

    .

--- Assets/02.Scripts/MummyRay.cs
+++ Assets/02.Scripts/MummyRay.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
 public class MummyRay : Agent
 {
@@ -14,7 +16,7 @@ public class MummyRay : Agent
 
     public override void Initialize()
     {
-        MaxStep = 50;
+        MaxStep = 5000;
 
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
@@ -32,5 +34,26 @@ public class MummyRay : Agent
         // 에이전트의 위치와 회전값을 설정
         tr.localPosition = new Vector3(Random.Range(-20.0f, 20.0f), 0.05f, Random.Range(-20.0f, 20.0f));
         tr.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+
+    }
+
+    /*
+        이산(Discrete)
+            Branch 0 정지/전진/후진 이동   => 0, 1, 2 (3개)
+            Branch 1 정지/왼쪽/오른쪽 회전  => 0, 1, 2 (3개)
+    */
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var actions = actionsOut.DiscreteActions;
+        actions.Clear(); // 배열을 모두 0으로 초기화
     }
 }
