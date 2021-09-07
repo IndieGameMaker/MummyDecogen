1cce81b5ec3b083ce12091c725ed09070cd87e77
Lee Jae Hyun <myleje@gmail.com>
2021-09-07 09:37:13 +0900

    .

--- Assets/02.Scripts/StageManager.cs
+++ Assets/02.Scripts/StageManager.cs
@@ -7,6 +7,32 @@ public class StageManager : MonoBehaviour
     public GameObject goodItem;
     public GameObject badItem;
 
+    [Range(10, 50)]
+    public int goodItemCount = 30;
+    [Range(10, 50)]
+    public int badItemCount = 20;
+
+    public List<GameObject> goodList = new List<GameObject>();
+    public List<GameObject> badList = new List<GameObject>();
+
+    public void InitStage()
+    {
+        // 기존에 생성된 모든 아이템을 삭제 및 List 초기화
+        foreach (var obj in goodList)
+        {
+            Destroy(obj);
+        }
+        foreach (var obj in badList)
+        {
+            Destroy(obj);
+        }
+
+        goodList.Clear();
+        badList.Clear();
+
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
